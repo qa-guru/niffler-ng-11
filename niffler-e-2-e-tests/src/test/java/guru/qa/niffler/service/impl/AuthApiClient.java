ba5e8196a1df2f6c47472883c40baabad908e55f
dtuchs <dtuchs@gmail.com>
2026-03-20 19:58:44 +0300

    Video #11.2  Oauth 2.0 login

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/AuthApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/AuthApiClient.java
@@ -1,8 +1,13 @@
 package guru.qa.niffler.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import guru.qa.niffler.api.AuthApi;
+import guru.qa.niffler.api.core.CodeInterceptor;
 import guru.qa.niffler.api.core.ThreadSafeCookieStore;
+import guru.qa.niffler.jupiter.extension.ApiLoginExtension;
 import guru.qa.niffler.service.RestClient;
+import guru.qa.niffler.utils.OAuthUtils;
+import lombok.SneakyThrows;
 import retrofit2.Response;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -14,8 +19,41 @@ public final class AuthApiClient extends RestClient {
   private final AuthApi authApi;
 
   public AuthApiClient() {
-    super("https://auth.niffler-stage.qa.guru/", true);
+    super(CFG.authUrl(), true, new CodeInterceptor());
     this.authApi = create(AuthApi.class);
+  }
+
+  @SneakyThrows
+  public String login(String username, String password) {
+    final String codeVerifier = OAuthUtils.generateCodeVerifier();
+    final String codeChallenge = OAuthUtils.generateCodeChallenge(codeVerifier);
+    final String redirectUri = CFG.frontUrl() + "authorized";
+    final String clientId = "client";
+
+    authApi.authorize(
+        "code",
+        clientId,
+        "openid",
+        redirectUri,
+        codeChallenge,
+        "S256"
+    ).execute();
+
+    authApi.login(
+        username,
+        password,
+        ThreadSafeCookieStore.INSTANCE.cookieValue("XSRF-TOKEN")
+    ).execute();
+
+    Response<JsonNode> tokenResponse = authApi.token(
+        clientId,
+        redirectUri,
+        "authorization_code",
+        ApiLoginExtension.getCode(),
+        codeVerifier
+    ).execute();
+
+    return tokenResponse.body().get("id_token").asText();
   }
 
   public Response<Void> register(String username, String password) throws IOException {
